548358d1849440f08f5b72dfc2ea3bd18435a9c1
ymstazaz <r37a45d@gmail.com>
2024-11-28 22:17:30 +0900

    Securityの導入

---
+++ src/main/java/jp/yamashita/raiseclick/SecurityConfig.java
@@ -0,0 +1,12 @@
+package jp.yamashita.raiseclick;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain
+}
