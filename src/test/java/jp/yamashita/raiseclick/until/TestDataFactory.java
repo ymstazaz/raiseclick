58cfa8eda31551e181a550d2d606be80fcd900b5
ymstazaz <r37a45d@gmail.com>
2024-12-13 20:18:55 +0900

    テストコードの実施（成功パターンのみ）

--- src/test/java/jp/yamashita/raiseclick/until/TestDataFactory.java
+++ src/test/java/jp/yamashita/raiseclick/until/TestDataFactory.java
@@ -1,6 +1,10 @@
 package jp.yamashita.raiseclick.until;
 
 
+import jp.yamashita.raiseclick.User;
+
 public class TestDataFactory {
-    public static final Faker faker = new Faker();
+    public static User createUser(){
+        return new User(1L, "太郎", "000@000","123456a","北海道","旭川市","秘密","10代","ROLE_USER");
+    }
 }
